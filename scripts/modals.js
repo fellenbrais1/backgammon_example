632a1cb77e4772419129f7f9e0b4db637a09f346
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2025-03-15 21:42:53 +0900

    Test changes

--- scripts/modals.js
+++ scripts/modals.js
@@ -505,7 +505,8 @@ export async function changeModalContent(tag = 'Challenge', data = '') {
       // const modalChallengeSection2 =
       //   document.querySelector('.challenge_section');
       // modalChallengeSection2.style.backgroundColor = 'red';
-      const challengeInformation2 = document.querySelector('.challenge_text');
+      const challengeInformation2 =
+        modalSection.querySelector('.challenge_text');
       challengeInformation2.textContent = `Challenge has been rejected!`;
       setTimeout(() => {
         removeModal();
